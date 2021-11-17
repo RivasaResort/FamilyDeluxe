5b0ca578001bb0b3993deabaef2e57d833145f7f
Sanju Kamat <sanjukamat123@gmail.com>
2021-11-17 19:23:19 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,282 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.38461259277603865,
+        "pitch": 0.27473221330082787,
+        "fov": 1.2016365735590553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.351010796966147,
+          "pitch": 0.06864744006501944,
+          "rotation": 1.5707963267948966,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.645993307615889,
+          "pitch": 0.09984402393687475,
+          "rotation": 1.5707963267948966,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.3592616215242952,
+          "pitch": 0.0638324386624678,
+          "rotation": 1.5707963267948966,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9950519043986059,
+          "pitch": -0.09138523187073488,
+          "title": "FAMILY DELUXE",
+          "text": "<div>Amenities&nbsp;</div><div><br></div><div>- AC</div><div>- TV</div><div>- Sanitized Rooms&nbsp;</div><div>- Daily Housekeeping&nbsp;</div><div>- Free Wifi</div><div>- Swimming pool</div><div>- In house Restaurant &amp; Bar</div><div>- Parking Facility&nbsp;</div><div>- 5 Min walk to the Beach</div><div>- Conference Hall&nbsp;</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.7904497032704416,
+        "pitch": 0.2378008988470821,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.089964606563793,
+          "pitch": 0.04038739578116157,
+          "rotation": 1.5707963267948966,
+          "target": "0-1"
+        },
+        {
+          "yaw": 2.7566817471727143,
+          "pitch": 0.0523679559075525,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.47762763313652,
+          "pitch": 0.04989860154958592,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1921501252797118,
+          "pitch": 0.08197222993642406,
+          "rotation": 14.137166941154074,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.6895634653923715,
+        "pitch": 0.3656464081594386,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3153770666012008,
+          "pitch": 0.09605549057013718,
+          "rotation": 4.71238898038469,
+          "target": "4-5"
+        },
+        {
+          "yaw": 2.6713760622686333,
+          "pitch": 0.04437731239105425,
+          "rotation": 1.5707963267948966,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.9843536122498442,
+        "pitch": 0.3388849095751745,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.540934250580098,
+          "pitch": 0.07458173121991862,
+          "rotation": 0,
+          "target": "3-4"
+        },
+        {
+          "yaw": 0.4190457059252566,
+          "pitch": 0.17418895043142868,
+          "rotation": 0,
+          "target": "5-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.163199747294996,
+        "pitch": 0.502428141775475,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3512645378566095,
+          "pitch": 0.07332460069556035,
+          "rotation": 4.71238898038469,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "FAMILY DELUXE ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
